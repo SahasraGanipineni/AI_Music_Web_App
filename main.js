d07e18d22f79c972b6d56af2b514b62b2294c075
SahasraGanipineni <87746901+SahasraGanipineni@users.noreply.github.com>
2021-12-12 10:55:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 HarryPorter_theme_song = "";
 PetPan_song = "";
+leftWristX = 0;
+leftWristy = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     HarryPorter_theme_song = loadSound("HarryPortter_theme_song.mp3");
@@ -12,6 +16,26 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose' , gotPoses);
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + " Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + " Right Wrist Y = " + rightWristY);
+    }
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
 }
 
 function draw() {
